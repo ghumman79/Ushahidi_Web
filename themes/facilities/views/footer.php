2ba24224cf798ebb67d6b59e2ce2c2def7b4fc68
Dale Zak <dalezak@gmail.com>
2012-04-18 16:29:56 -0600

    Removed bottom boxes

--- themes/facilities/views/footer.php
+++ themes/facilities/views/footer.php
@@ -1,8 +1,6 @@
 </div>
 </div>
 <!-- / main body -->
-</div>
-<!-- / wrapper -->
 
 <!-- footer -->
 <div id="footer" class="clearingfix">
@@ -14,7 +12,7 @@
 
         <!-- footer credits -->
         <div class="footer-credits">
-            <a href="http://www.ushahidi.com" title="Ushahidi"><img src="../images/footer-logo.png" alt="Ushahidi" style="vertical-align:middle" /></a>
+            <a href="http://www.ushahidi.com" title="Ushahidi"><img src="../images/footer-logo.png" alt="Ushahidi" /></a>
         </div>
         <!-- / footer credits -->
 
@@ -24,7 +22,6 @@
                 <li><a class="item1" href="<?php echo url::site(); ?>"><?php echo Kohana::lang('ui_main.home'); ?></a></li>
                 <li><a href="/feed/"><?php echo Kohana::lang('ui_main.rss'); ?></a></li>
                 <?php
-                // Action::nav_main_bottom - Add items to the bottom links
                 Event::run('ushahidi_action.nav_main_bottom');
                 ?>
             </ul>
@@ -40,7 +37,6 @@
 
 <?php
 echo $footer_block;
-// Action::main_footer - Add items before the </body> tag
 Event::run('ushahidi_action.main_footer');
 ?>
 </body>
