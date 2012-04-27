d098c8fa3943f3b0841c39ea106800567833d9ea
Gilbert <gbat2009@gmail.com>
2012-04-27 10:28:50 -0600

    layout of details page

--- themes/facilities/views/reports_view.php
+++ themes/facilities/views/reports_view.php
@@ -1,9 +1,45 @@
-<div id="left-col" style="float: left; width:45%; background: #000000" >
+<div id="main" class="report_detail">
 
-    <div class="report-map" style="margin-top: 120px; margin-left: 20px">
-    <div class="map-holder" id="map"></div>
+<div id="left-col" style="float: left; width:45%; background: transparent" >
 
-        <div class="<?php if( count($incident_photos) > 0 || count($incident_videos) > 0){ echo "report-media";}?>">
+    <div class="report-map" style="margin-top: 100px; margin-left: 20px; position: relative;">
+    <div class="map-holder" id="map">
+        <div class="report-category-list">
+            <p>
+                <?php
+                foreach($incident_category as $category)
+                {
+
+                    // don't show hidden categoies
+                    if($category->category->category_visible == 0)
+                    {
+                        continue;
+                    }
+
+                    if ($category->category->category_image_thumb)
+                    {
+                        ?>
+                        <a href="<?php echo url::site()."reports/?c=".$category->category->id; ?>"><span class="r_cat-box" style="background:transparent url(<?php echo url::base().Kohana::config('upload.relative_directory')."/".$category->category->category_image_thumb; ?>) 0 0 no-repeat;">&nbsp;</span> <?php echo $category->category->category_title; ?></a>
+
+                        <?php
+                    }
+                    else
+                    {
+                        ?>
+                        <a href="<?php echo url::site()."reports/?c=".$category->category->id; ?>"><span class="r_cat-box" style="background-color:#<?php echo $category->category->category_color; ?>">&nbsp;</span> <?php echo $category->category->category_title; ?></a>
+                        <?php
+                    }
+                }
+                ?>
+            </p>
+            <?php
+            // Action::report_meta - Add Items to the Report Meta (Location/Date/Time etc.)
+            Event::run('ushahidi_action.report_meta', $incident_id);
+            ?>
+        </div>
+    </div>
+
+        <div class="<?php if( count($incident_photos) > 0 || count($incident_videos) > 0){ echo "report-media";}?>" style="margin-top: 50px">
         <?php
         // if there are images, show them
         if( count($incident_photos) > 0 )
@@ -35,17 +71,133 @@
     </div>
 
 
+        <!-- start news source link -->
+        <?php if( count($incident_news) > 0 ) { ?>
+        <div class="credibility">
+            <h5><?php echo Kohana::lang('ui_main.reports_news');?></h5>
+            <?php
+            foreach( $incident_news as $incident_new)
+            {
+                ?>
+                <a href="<?php echo $incident_new; ?> " target="_blank"><?php
+                    echo $incident_new;?></a>
+                <br/>
+                <?php
+            }
+            ?>
+        </div>
+        <?php } ?>
+        <!-- end news source link -->
+
+        <!-- start additional fields -->
+        <?php if(strlen($custom_forms) > 0) { ?>
+        <div class="credibility">
+            <h5><?php echo Kohana::lang('ui_main.additional_data');?></h5>
+            <?php
+
+            echo $custom_forms;
+
+            ?>
+            <br/>
+        </div>
+        <?php } ?>
+        <!-- end additional fields -->
+
+        <?php if ($features_count)
+    {
+        ?>
+        <br /><br /><h5><?php echo Kohana::lang('ui_main.reports_features');?></h5>
+        <?php
+        foreach ($features as $feature)
+        {
+            echo ($feature->geometry_label) ?
+                "<div class=\"feature_label\"><a href=\"javascript:getFeature($feature->id)\">$feature->geometry_label</a></div>" : "";
+            echo ($feature->geometry_comment) ?
+                "<div class=\"feature_comment\">$feature->geometry_comment</div>" : "";
+        }
+    }?>
+
+        <div class="credibility">
+            <table class="rating-table" cellspacing="0" cellpadding="0" border="0">
+                <tr>
+                    <td><?php echo Kohana::lang('ui_main.credibility');?>:</td>
+                    <td><a href="javascript:rating('<?php echo $incident_id; ?>','add','original','oloader_<?php echo $incident_id; ?>')"><img id="oup_<?php echo $incident_id; ?>" src="<?php echo url::file_loc('img'); ?>media/img/up.png" alt="UP" title="UP" border="0" /></a></td>
+                    <td><a href="javascript:rating('<?php echo $incident_id; ?>','subtract','original')"><img id="odown_<?php echo $incident_id; ?>" src="<?php echo url::file_loc('img'); ?>media/img/down.png" alt="DOWN" title="DOWN" border="0" /></a></td>
+                    <td><a href="" class="rating_value" id="orating_<?php echo $incident_id; ?>"><?php echo $incident_rating; ?></a></td>
+                    <td><a href="" id="oloader_<?php echo $incident_id; ?>" class="rating_loading" ></a></td>
+                </tr>
+            </table>
+        </div>
+
+        <?php
+        // Action::report_extra - Allows you to target an individual report right after the description
+        Event::run('ushahidi_action.report_extra', $incident_id);
+        ?>
+    <div style="margin-bottom: 40px">
+        <?php
+        // Filter::comments_block - The block that contains posted comments
+        Event::run('ushahidi_filter.comment_block', $comments);
+        echo $comments;
+        ?>
+
+        <?php
+        // Filter::comments_form_block - The block that contains the comments form
+        Event::run('ushahidi_filter.comment_form_block', $comments_form);
+        echo $comments_form;
+        ?>
     </div>
+
+    </div>
+
+
 </div>
 
 
-    <div id="right-col" style="margin-top: 120px; float: right; padding-right: 20px; width:45%; background: #ff0000">
-        <div class="report-description-text" >
+</div>
+
+
+    <div id="right-col" style="margin-top: 80px; float: right; padding-right: 20px; width:45%; background: transparent">
+
+
+        <div class="report-category-list">
+            <p>
+                <?php
+                foreach($incident_category as $category)
+                {
+
+                    // don't show hidden categoies
+                    if($category->category->category_visible == 0)
+                    {
+                        continue;
+                    }
+
+                    if ($category->category->category_image_thumb)
+                    {
+                        ?>
+                        <a href="<?php echo url::site()."reports/?c=".$category->category->id; ?>"><span class="r_cat-box" style="background:transparent url(<?php echo url::base().Kohana::config('upload.relative_directory')."/".$category->category->category_image_thumb; ?>) 0 0 no-repeat;">&nbsp;</span> <?php echo $category->category->category_title; ?></a>
+
+                        <?php
+                    }
+                    else
+                    {
+                        ?>
+                        <a href="<?php echo url::site()."reports/?c=".$category->category->id; ?>"><span class="r_cat-box" style="background-color:#<?php echo $category->category->category_color; ?>">&nbsp;</span> <?php echo $category->category->category_title; ?></a>
+                        <?php
+                    }
+                }
+                ?>
+            </p>
+            <?php
+            // Action::report_meta - Add Items to the Report Meta (Location/Date/Time etc.)
+            Event::run('ushahidi_action.report_meta', $incident_id);
+            ?>
+        </div>
+
+        <div class="report-description-text" style="padding-top:2px " >
             <h5><?php echo Kohana::lang('ui_main.reports_description');?></h5>
             <?php echo $incident_description; ?>
             <br/>
         </div>
-
 
         <div class="report-additional-reports" style="margin-right: 40px">
             <h4><?php echo Kohana::lang('ui_main.additional_reports');?></h4>
@@ -61,4 +213,4 @@
 
     </div>
 
-
+</div>
