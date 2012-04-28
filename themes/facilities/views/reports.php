aee6909451958bd0dadce8f99e7a26a9fef90d07
Dale Zak <dalezak@gmail.com>
2012-04-28 14:08:43 -0600

    Added category filtering

--- themes/facilities/views/reports.php
+++ themes/facilities/views/reports.php
@@ -6,7 +6,6 @@
         <ul>
             <?php echo $category_tree_view; ?>
         </ul>
-        <a href="#" id="applyFilters" class="filter-button"><?php echo Kohana::lang('ui_main.filter_reports'); ?></a>
     </div>
 
     <?php
@@ -20,4 +19,31 @@
         Event::run('ushahidi_filter.report_stats', $report_stats);
         echo $report_stats; ?>
     </div>
+
+    <script type="text/javascript">
+        $(function(){
+            $(".cat_selected").click(function(){
+                if ($(this).hasClass("selected")) {
+                    $(this).removeClass("selected");
+                    $(this).parent().removeClass("selected");
+                }
+                else {
+                    $(this).addClass("selected");
+                    $(this).parent().addClass("selected");
+                }
+                var category_ids = [];
+                $.each($("#categories li a.selected"), function(i, item){
+                    itemId = item.id.substring("filter_link_cat_".length);
+                    category_ids.push(itemId);
+                });
+                if (category_ids.length > 0) {
+                    urlParameters["c"] = category_ids;
+                }
+                else {
+                    urlParameters = [];
+                }
+                fetchReports();
+            });
+        });
+    </script>
 </div>
