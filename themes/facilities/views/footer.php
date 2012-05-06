8f56c1137b0c699bb343d757c75297f5c48eac66
Dale Zak <dalezak@gmail.com>
2012-05-06 11:18:44 -0600

    Added secondary pages to footer

--- themes/facilities/views/footer.php
+++ themes/facilities/views/footer.php
@@ -8,19 +8,21 @@
     <!-- footer menu -->
     <div class="footermenu">
         <ul class="clearingfix">
-            <li><a class="item1" href="/main"><?php echo Kohana::lang('ui_main.home'); ?></a></li>
-            <li><a href="/reports"><?php echo Kohana::lang('ui_main.reports'); ?></a></li>
+            <li><a class="item1" title="<?php echo Kohana::lang('ui_main.home'); ?>" href="/main"><?php echo Kohana::lang('ui_main.home'); ?></a></li>
+            <li><a title="<?php echo Kohana::lang('ui_main.reports'); ?>" href="/reports"><?php echo Kohana::lang('ui_main.reports'); ?></a></li>
             <?php if(Kohana::config('settings.allow_reports')) { ?>
-                <li><a href="<?php echo url::site()."reports/submit"; ?>"><?php echo Kohana::lang('ui_main.submit'); ?></a></li>
+                <li><a title="<?php echo Kohana::lang('ui_main.submit'); ?>" href="<?php echo url::site()."reports/submit"; ?>"><?php echo Kohana::lang('ui_main.submit'); ?></a></li>
             <?php } ?>
             <?php if(Kohana::config('settings.allow_alerts')) { ?>
-                <li><a href="<?php echo url::site()."alerts"; ?>"><?php echo Kohana::lang('ui_main.alerts'); ?></a></li>
+                <li><a title="<?php echo Kohana::lang('ui_main.alerts'); ?>" href="<?php echo url::site()."alerts"; ?>"><?php echo Kohana::lang('ui_main.alerts'); ?></a></li>
             <?php } ?>
             <?php if(Kohana::config('settings.site_contact_page')) { ?>
-                <li><a href="<?php echo url::site()."contact"; ?>"><?php echo Kohana::lang('ui_main.contact'); ?></a></li>
+                <li><a title="<?php echo Kohana::lang('ui_main.contact'); ?>" href="<?php echo url::site()."contact"; ?>"><?php echo Kohana::lang('ui_main.contact'); ?></a></li>
             <?php } ?>
-            <?php Event::run('ushahidi_action.nav_main_bottom'); ?>
-            <li><a href="/feed/"><?php echo Kohana::lang('ui_main.rss'); ?></a></li>
+            <?php foreach (ORM::factory('page')->where('page_active', '1')->find_all() as $page) { ?>
+                <li><a title="<?php echo $page->page_tab; ?>" href="<?php echo url::site()."page/index/".$page->id; ?>"><?php echo $page->page_tab; ?></a></li>
+            <?php }?>
+            <li><a title="<?php echo Kohana::lang('ui_main.rss'); ?>" href="/feed/"><?php echo Kohana::lang('ui_main.rss'); ?></a></li>
         </ul>
         <?php if($site_copyright_statement != '') { ?>
             <span class="copyright"><?php echo $site_copyright_statement; ?></span>
